4e9ca99456c81750cf92a8c18895c212ddc95b2d
Andrew Kim <19kima@elmbrookstudents.org>
2018-02-21 08:30:03 -0600

    Merge branch 'feature' into dev
    coded testclass1

---
+++ greet/src/greet/TestClass.java
@@ -0,0 +1,12 @@
+package greet;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter eric = new Greeter("eric chen", 17);
+		System.out.println(eric.toString());
+		
+	}
+
+}
