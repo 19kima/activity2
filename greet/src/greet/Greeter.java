71270c811d5fca23644b0b4aea5741e54aa19365
Andrew Kim <19kima@elmbrookstudents.org>
2018-02-21 08:24:20 -0600

    coded greeter class

--- greet/src/greet/Greeter.java
+++ greet/src/greet/Greeter.java
@@ -1,5 +1,17 @@
 package greet;
 
 public class Greeter {
-
+	private String name;
+	private int age;
+	public Greeter() {
+		name = "John Park";
+		age = 12;
+	}
+	public Greeter(String xName, int xAge) {
+		name = xName;
+		age = xAge;
+	}
+	public String toString() {
+		return ("Greeter object: \nname: " + name + "\nage: " + age);
+	}
 }
